35bf424ab6f4f025e95492e9efa1d21e7ca1eaa0
Chelsea David <cryan17@cnm.edu>
2017-07-12 14:00:02 -0600

    added table, ul and ol

--- index.php
+++ index.php
@@ -6,5 +6,43 @@
 	</head>
 	<body>
 		<h1>This is my web page</h1>
+		<!-- Created table cats and dogs -->
+		<table>
+			<tr>
+				<th>Cats</th>
+				<th>Dogs</th>
+			</tr>
+			<tr>
+				<td>Persian</td>
+				<td>Beagle</td>
+			</tr>
+			<tr>
+				<td>Main Coon</td>
+				<td>Rottweiler</td>
+			</tr>
+			<tr>
+				<td>Siamese</td>
+				<td>German Shepard</td>
+			</tr>
+		</table>
+
+		<!-- This is an HTML comment -->
+
+		<h2>A few of my favorite things</h2>
+		<ul>
+			<li>Archery</li>
+			<li>Roller Derby</li>
+			<li>Dogs</li>
+			<li>Dia De Los Muertos</li>
+		</ul>
+
+		<h2>Things to Do Today</h2>
+		<ol>
+			<li>Feed the dog</li>
+			<li>Make dinner</li>
+			<li>Water the plants</li>
+			<li>Run to the post office</li>
+		</ol>
+
 	</body>
 </html>
